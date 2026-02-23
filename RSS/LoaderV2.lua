8c2d7720b58162a2e7d69e956e72f43b4653c147
LOL5678906 <ericmauricegreen2006@gmail.com>
2026-02-22 18:55:44 -0600

    Delete RSS/LoaderV2.lua

--- RSS/LoaderV2.lua
+++
@@ -1,3 +0,0 @@
-game:GetService("Players").LocalPlayer:Kick(
-    "This script is now private. To request access, contact Nate in the Discord: https://discord.gg/Wd78JMWb or purchase permanent access for $3."
-)
