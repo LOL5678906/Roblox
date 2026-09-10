73be2349556a2c0786155c8dd87d83436a2f28c2
LOL5678906 <ericmauricegreen2006@gmail.com>
2026-09-10 16:13:42 -0500

    Update LoaderV2.lua

--- Basketball Stars 3/LoaderV2.lua
+++ Basketball Stars 3/LoaderV2.lua
@@ -1,10 +1,11 @@
 -- // LOADER \\
+
 local b = game:GetService("HttpService")
 
-local Loader = "https://gist.githubusercontent.com/LOL5678906/ec26ae3c3da550efb1c76290c863b7b5/raw/fbd49c5d769c61ef1261ab3588eb1946d389262b/BS3.Json"
+local Loader = "https://gist.githubusercontent.com/LOL5678906/ec26ae3c3da550efb1c76290c863b7b5/raw/0a0647f2e300d3dd3455af86a2596cc9bb7bc2b7/BS3.Json"
 LoaderV1 = Loader .. "?t=" .. tick()
 
-local K = table.concat({ "akxWc1Ntak13d0pOTG94R1pwaG5sZjI0U1FrcDlBcDY=" })
+local K = table.concat({ "MXg2TTE0YjJvMUZwRFFqeUNlWGp0cDU5bFhZMjhkNzU=" })
 
 local ok1, raw = pcall(function()
     return game:HttpGet(LoaderV1)
